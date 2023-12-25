42a5120efe2cd7224a83a5ae77c7a52ca0874da7
unknown <luis122448@gmail.com>
2023-12-25 15:33:54 -0500

    Update enviroment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  API_URL: 'http://localhost:8080/app-project',
+  production: true,
+  API_URL: 'https://smart-shell.luis122448.dev/app-project',
   PATH_BILLING: '/v1/facturacion',
   PATH_BUSINESS_PARTNER: "/business-partner"
 };
