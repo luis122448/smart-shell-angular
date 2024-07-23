1a13df7478e973e22a540f8b9f4ba2ef02b4b476
LUIS ANTONIO CALVO QUISPE <luis122448@gmail.com>
2024-07-22 23:15:06 -0500

    feat(deploy): update angular enviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  API_URL: 'https://smart-shell.luis122448.dev/app-project',
+  API_URL: process.env['API_URL'],
   PATH_BILLING: '/v1/facturacion',
   PATH_BUSINESS_PARTNER: "/business-partner",
-  API_SUNAT_TOKEN: 'lCSzUmWQLRZT4ytYL3EgHCKTBh7K2dswFaFjsB1nKkq6RIaGB0AuRD2qvtsxm8q4'
+  API_SUNAT_TOKEN: process.env['API_SUNAT_TOKEN']
 };
