097d324cf202b084c293d56b8d9b9ea0e9b73c5d
luis122448 <luis122448@gmail.com>
2024-09-05 00:11:25 +0000

    migrate(app): migrate app to angular 16, angular/cli

--- src/app/guards/redirect.guard.ts
+++ src/app/guards/redirect.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { TokenService } from 'src/app/auth/services/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RedirectGuard implements CanActivate {
+export class RedirectGuard  {
 
 constructor(
   private tokenService: TokenService,
